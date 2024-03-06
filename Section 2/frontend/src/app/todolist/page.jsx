46ae65c7ba37a7e2bc5fddd7a7955ca15692ca65
m-triple-m <triplem656@gmail.com>
2024-03-06 11:29:22 +0530

    ..

--- Section 2/frontend/src/app/todolist/page.jsx
+++ Section 2/frontend/src/app/todolist/page.jsx
@@ -1,13 +1,27 @@
 import React from 'react'
 
 const TodoList = () => {
+
+    const addNewTask = (e) => {
+        console.log(e.code);
+    }
+
     return (
         <div className='vh-100 bg-primary-subtle'>
 
             <div className='container py-5'>
                 <h1 className='display-3 text-center fw-bold'>ToDo List</h1>
                 <div className='card shadow'>
-                    <div className="card-header"></div>
+                    <div className="card-header">
+
+                        <input
+                            type="text"
+                            className='form-control border-primary border-3'
+                            placeholder='Press Enter to add Task'
+                            onKeyDown={}
+                        />
+
+                    </div>
                     <div className="card-body"></div>
                 </div>
             </div>
